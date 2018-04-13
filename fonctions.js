c38511725ca7f06f948ed996f52db407ba7b427b
Jennifer Van Kelst <35732706+jennifervankelst@users.noreply.github.com>
2018-04-13 09:21:33 +0200

    Fin des exercices sur les fonctions

---
+++ fonctions.js
@@ -0,0 +1,70 @@
+
+
+
+//soustraction 
+
+
+let nombreA = prompt("Rentrez votre premier nombre");
+console.log(nombreA)
+
+let nombreB = prompt("Rentrez votre deuxième nombre");
+console.log(nombreB)
+
+let result = (nombreA - nombreB)
+console.log(result)
+
+
+//Multiplication
+
+
+let nombreA = prompt("Rentrez votre premier nombre");
+console.log(nombreA)
+
+let nombreB = prompt("Rentrez votre deuxième nombre");
+console.log(nombreB)
+
+let result = (nombreA * nombreB)
+console.log(result)
+
+
+//Division 
+
+
+let nombreA = prompt("Rentrez votre premier nombre");
+console.log(nombreA)
+
+let nombreB = prompt("Rentrez votre deuxième nombre");
+console.log(nombreB)
+
+let result = (nombreA / nombreB)
+console.log(result)
+
+
+
+// Poucentage
+
+
+let nombreA = prompt("veuillez entrez votre premier nombre");
+console.log(nombreA)
+
+let pourcentage = prompt("veuillez entrez votre pourcentage");
+console.log(pourcentage)
+
+let result = (nombreA /100 * pourcentage)
+console.log (pourcentage,"%", "de", nombreA, "est", result)
+
+
+
+//Calcul vitesse
+
+
+let distance = prompt("Rentrez votre distance en KM");
+console.log(distance)
+
+let heure = prompt("Rentrez votre temps en HEURE");
+console.log(heure)
+
+
+let result = (distance / heure)
+console.log("Vous faites une distance de", distance, "km en",heure, "heure vous roulez donc à du",result,"KM/H")
+
