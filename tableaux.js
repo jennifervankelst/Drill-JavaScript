0a143709677f6d423597cf924af3a0531de5bd66
Jennifer Van Kelst <35732706+jennifervankelst@users.noreply.github.com>
2018-04-13 16:35:01 +0200

    Fin des exercices tableaux

---
+++ tableaux.js
@@ -0,0 +1,25 @@
+
+
+//Exercice 1
+
+
+let tableau = [1,2,3,4,5,6,7,8,9];
+let result = 0;
+for(let valeur in tableau) {result += tableau[valeur];}
+
+
+//Exercice 3 
+
+
+let jeu = ["Mario", "Luigi", "Peach"];
+    console.log(jeu);
+
+
+    console.log(jeu.push("Bowser"));
+        Array [ "Mario", "Luigi", "Peach" ]
+
+    console.log(jeu);
+        Array ["Mario", "Luigi", "Peach", "Bowser"]
+
+
+        
